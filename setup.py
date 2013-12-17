ff9d56dab80f573e4457382c1271bec7a4afa263
Daniel Havlik <dh@gocept.com>
2013-12-17 15:17:07 +0100

    Back to development: 0.3

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ install_requires = [
     ]
 
 setup(name='pyramid_clamav',
-      version='0.2',
+      version='0.3.dev0',
       description=('A tween checking file uploads for viruses using clamav.'),
       long_description=README + '\n\n' + CHANGES,
       classifiers=[

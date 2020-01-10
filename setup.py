08f27c843f6f5f79d529a52e898073191280b1a0
Sebastian Wehrmann <sebastian@wehrmann.de>
2020-01-10 14:50:38 +0100

    Preparing release 0.6

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ install_requires = [
     ]
 
 setup(name='pyramid_clamav',
-      version='0.6.dev0',
+      version='0.6',
       description=('A tween checking file uploads for viruses using clamav.'),
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
